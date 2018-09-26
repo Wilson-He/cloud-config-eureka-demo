d8f526e2711982fd88b47e1d139d4b8e6370ef89
Wilson-He <845023508@qq.com>
2018-09-27 01:37:27 +0800

    update tomcat

--- cloud-eureka/src/main/java/per/wilson/eureka/EurekaServer.java
+++ cloud-eureka/src/main/java/per/wilson/eureka/EurekaServer.java
@@ -1,8 +1,14 @@
 package per.wilson.eureka;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author: hewx
@@ -11,9 +17,21 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer {
+@Api
+@RestController
+@RequestMapping("/")
+public class EurekaServer extends SpringBootServletInitializer {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 
-  public static void main(String[] args) {
-    SpringApplication.run(EurekaServer.class);
-  }
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(SpringApplication.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class);
+    }
 }
